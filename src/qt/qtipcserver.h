8f4dd009660d3b713161594cdd06fbec038781f5
Ratio <ratio@mail.com>
2015-12-11 20:09:11 +0100

    The legendary first

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ratio-qt message queue name
+#define BITCOINURI_QUEUE_NAME "RatioURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
